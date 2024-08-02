2455a4df4b51a3845b69d7cab59a9e19dd572dca
Karl Honrado <honradokarljohn@gmail.com>
2024-08-02 14:53:10 +0800

    :tada: initial commit

---
+++ packages/honradokarl/laravel-facebook-graph-api/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'honradokarl/laravel-facebook-graph-api',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'honradokarl/laravel-facebook-graph-api' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
